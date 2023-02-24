6ae79fcc8c80e907a479323bd565a5e29e6615ac
AlanCanalesM <alan.canales@micorreo.upp.edu.mx>
2023-02-23 23:10:13 -0600

    I made event detail page

--- src/pages/events/[eventid].js
+++ src/pages/events/[eventid].js
@@ -1,10 +1,34 @@
+import EventContent from "components/event-detail/event-content";
+import EventLogistics from "components/event-detail/event-logistics";
+import EventSummary from "components/event-detail/event-summary";
+import { getEventById } from "dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 function EventDetailPage() {
+  const router = useRouter();
+  const id = router.query.eventid;
+
+  const event = getEventById(id);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>The Event Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
-
 
 export default EventDetailPage;
